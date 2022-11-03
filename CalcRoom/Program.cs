c7e36e471697ee8ca90d8e804324036cf0fc46ae
ryheinz <ryan.heinz@gmail.com>
2022-11-03 18:16:22 +0100

    square version 1

--- CalcRoom/Program.cs
+++ CalcRoom/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace CalcRoom
+﻿using System;
+
+namespace ConsoleApp1 // area cost calculation. 
 {
-    internal class Program
+    class pgm
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int length, width, area, cost, price;
+            Console.Write("Enter length of room in feet: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter width of room in feet:");
+            width = Convert.ToInt32(Console.ReadLine());
+            area = length * width;
+            Console.Write("Enter price of square feet: ");
+            price = Convert.ToInt32(Console.ReadLine());
+            cost = (length * width) * price;
+            Console.WriteLine("Floor is " + area + " square feet.");
+            Console.WriteLine("Cost for room is " + cost + ".");
+            Console.ReadLine();
         }
     }
 }
