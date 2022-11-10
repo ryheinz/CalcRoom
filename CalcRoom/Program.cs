dd8aa3430e1d2c1dfe469d7e285282a0b6d956ce
ryheinz <ryan.heinz@gmail.com>
2022-11-10 22:30:33 +0100

    Update Program.cs
    2nd attempt

--- CalcRoom/Program.cs
+++ CalcRoom/Program.cs
@@ -2,19 +2,18 @@
 
 namespace CalcRoom // area cost calculation. 
 {
-    class CalcRoom.pgm;
+    class CalcRoom
     {
-        public static void Main();
+        public static void Main()
         {
-            double length, width, area, cost, price;
             Console.Write("Enter length of room in feet: ");
-            length = Convert.ToInt32(Console.ReadLine());
+            double length = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter width of room in feet:");
-            width = Convert.ToInt32(Console.ReadLine());
-            area = length * width;
+            double width = Convert.ToInt32(Console.ReadLine());
+            double area = length * width;
             Console.Write("Enter price of square feet: ");
-            price = Convert.ToInt32(Console.ReadLine());
-            cost = (length * width) * price;
+            double price = Convert.ToInt32(Console.ReadLine());
+            double cost = (length * width) * price;
             Console.WriteLine("Floor is " + area + " square feet.");
             Console.WriteLine("Cost for room is " + cost + ".");
             Console.ReadLine();
